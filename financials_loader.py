6bb5cc90311526064c94aff369f39ddc257eecea
Michael <s.michaelru@gmail.com>
2020-06-03 23:49:42 -0400

    Changed FinancialsLoader format
    Signed-off-by: Michael <s.michaelru@gmail.com>

--- financials_loader.py
+++ financials_loader.py
@@ -1,29 +1,26 @@
 import csv
-import pandas as pd
+from pandas import read_csv
 import os
 
 class FinancialsLoader:
     def __init__(self, ticker):
         self.ticker = ticker
-        self.balance_sheet = None
-        self.cashflow = None
-        self.income_statement = None
-        self.key_ratios = None
         self.dirname = os.path.dirname(__file__)
 
     def load_balance_sheet(self):
         filename = os.path.join(self.dirname, 'data/{} {}.csv'.format(self.ticker, "Balance Sheet"))
-        self.balance_sheet = pd.read_csv(filename)
+        return read_csv(filename)
+
 
     def load_cashflow(self):
         filename = os.path.join(self.dirname, 'data/{} {}.csv'.format(self.ticker, "Cash Flow"))
         print(filename)
-        self.cashflow = pd.read_csv(filename)
+        return read_csv(filename)
 
     def load_income_statement(self):
         filename = os.path.join(self.dirname, 'data/{} {}.csv'.format(self.ticker, "Income Statement"))
-        self.income_statement = pd.read_csv(filename)
+        return read_csv(filename)
 
     def load_key_ratios(self):
         filename = os.path.join(self.dirname, 'data/{} {}.csv'.format(self.ticker, "Key Ratios"))
-        self.key_ratios = pd.read_csv(filename)
+        return read_csv(filename)
--- get_data.py
+++ get_data.py
@@ -1,15 +1,13 @@
 from financials_loader import FinancialsLoader
+import matplotlib.pyplot as plt
 
 msft_fl = FinancialsLoader("MSFT")
 
-msft_fl.load_balance_sheet()
-print(msft_fl.balance_sheet)
+msft_cashflow = msft_fl.load_cashflow()
+# print(msft_fl.cashflow)
 
-msft_fl.load_cashflow()
-print(msft_fl.cashflow)
+print(msft_cashflow.loc["Free cash flow"])
 
-msft_fl.load_income_statement()
-print(msft_fl.income_statement)
-
-msft_fl.load_key_ratios()
-print(msft_fl.key_ratios)
+# plt.figure
+# plt.plot(msft_cashflow["Fiscal year ends in June. USD in millions except per share data."], msft_fl.cashflow.loc["Free cash flow"]
+# plt.show()
